40295560038aaaf00998923be7b2c29ba6a33237
Jose Dapena Paz <j.dapena@lgepartner.com>
2021-07-27 18:40:34 +0200

    [op][upsable][suspend] Do not crash destroying a page if ScopedPageSuspend is set.
    :Release Notes:
    Avoid certain crashes detected on closing a web application.
    
    :Detailed Notes:
    In the destructor of AppRuntimeRenderFrameObserver, we may be
    destroying the owned ScopedPagePauser in case it is set. If the frame
    observer is destroyed in the handler of Page destruction
    (Page::WillBeDestroyed) then we can get a crash in LocalFrame, as
    SetContextPaused will use frame_scheduler_, that will be already reset
    at that point.
    
    To prevent that situation, we only use the frame scheduler if the frame
    is not detached.
    
    :Testing Performed:
    Stability 15 hours test.
    
    :QA Notes:
    Test-cases: QWT-3872
    
    :Issues Addressed:
    [WRN-4480] Port : [op][upsable][suspend] Do not crash destroying a page
    if ScopedPageSuspend is set.
    [WRN-3564] [RDX_CRASH] /usr/bin/WebAppMgr
    
    Change-Id: Ia768831598a5d644d07763040dff8a5c6d7c137c
    Originally-Reviewed-on: http://gpro.lge.com/c/webosose/chromium91/+/315884
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium91/+/317620
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Youngsoo Choi <youngsoo.choi@lge.com>
    Tested-by: Youngsoo Choi <youngsoo.choi@lge.com>

--- src/third_party/blink/renderer/core/frame/local_frame.cc
+++ src/third_party/blink/renderer/core/frame/local_frame.cc
@@ -2526,6 +2526,12 @@ void LocalFrame::SetContextPaused(bool is_paused) {
 
   GetDocument()->Fetcher()->SetDefersLoading(GetLoadDeferType());
   Loader().SetDefersLoading(GetLoadDeferType());
+
+  // An ScopedPagePauser could be finished while any page is
+  // destroyed. It would be possible frame_scheduler_ would be
+  // already reset at this point.
+  if (!IsAttached())
+    return;
   // TODO(altimin): Move this to PageScheduler level.
   GetFrameScheduler()->SetPaused(is_paused);
 }
