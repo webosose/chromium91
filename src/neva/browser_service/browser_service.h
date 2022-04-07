2b3ec25c82e9c007a4b99de3adae734d43d080d1
Bharath Patil <bharath.patil@lge.com>
2022-04-07 19:28:37 +0530

    [op][n_upsable][app_shell] implement neva browser service
    :Release Notes:
    implement neva browser service to enable for integration of new browser
    features
    
    :Detailed Notes:
    Implemented neva browser service as module to support the new browser
    features.
    
    This will provide the renderer browser communication interface for
    integrating features which will be using injection approach.
    
    :Testing Performed:
    Build Tested OK.
    1. Build Neva within webOS/OSE using chromium build.
    2. Check that enact browser application works fine.
    
    :QA Notes:
    
    :Issues Addressed:
    [WRN-20131] Web Browser maintenance for N version
    
    Change-Id: Ibefebbd7788b45fa0a11b041c85e0259da9934fa
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium91/+/328421
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

---
+++ src/neva/browser_service/browser_service.h
@@ -0,0 +1,40 @@
+// Copyright 2022 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef NEVA_BROWSER_SERVICE_BROWSER_SERVICE_H_
+#define NEVA_BROWSER_SERVICE_BROWSER_SERVICE_H_
+
+#include "base/component_export.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+
+namespace browser {
+
+COMPONENT_EXPORT(BROWSER_SERVICE)
+class BrowserService {
+public:
+static BrowserService* GetBrowserService();
+
+private:
+  friend struct base::DefaultSingletonTraits<BrowserService>;
+  BrowserService(){}
+  BrowserService(const BrowserService&) = delete;
+
+  ~BrowserService(){}
+};
+
+}  // namespace browser
+
+#endif  // NEVA_BROWSER_SERVICE_BROWSER_SERVICE_H_
